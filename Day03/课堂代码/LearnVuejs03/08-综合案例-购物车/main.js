d6b017d33d02869b83bd3dcf2753224c7df56d4e
dmdkhao <1015835653@qq.com>
2021-02-16 14:04:07 +0800

    第二次提交Day03(内容为Day03的代码完整版)

---
+++ Day03/课堂代码/LearnVuejs03/08-综合案例-购物车/main.js
@@ -0,0 +1,63 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    books: [
+      {
+        id: 1001,
+        name: '《算法导论》',
+        date: '2006-9',
+        price: 85.00,
+        count: 1
+      },
+      {
+        id: 1002,
+        name: '《UNIX编程艺术》',
+        date: '2006-9',
+        price: 59.00,
+        count: 1
+      },
+      {
+        id: 1003,
+        name: '《编程珠玑》',
+        date: '2006-9',
+        price: 39.00,
+        count: 1
+      },
+      {
+        id: 1004,
+        name: '《代码大全》',
+        date: '2006-9',
+        price: 128.50,
+        count: 1
+      },
+
+    ]
+  },
+  methods: {
+    increment(index){
+      this.books[index].count++;
+    },
+    decrement(index){
+      this.books[index].count--;
+    },
+    removeBook(index){
+      this.books.splice(index,1);
+    }
+  },
+  computed: {
+    totalPrice(){
+      let totalPrice = 0;
+      for(let i of this.books){
+        totalPrice +=i.count * i.price;
+      }
+      return totalPrice;
+    }
+  },
+  filters: {
+    finalPrice(price){
+      return "￥" + price.toFixed(2);
+    }
+  },
+
+
+})
