405837ce8b48395234bcbac59571126634ceca9f
dmdkhao <1015835653@qq.com>
2021-02-25 12:58:47 +0800

    webpack配置及loader

---
+++ Day04/课堂代码/LearnVuejs04/01-webpack的使用/02-webpack的配置/src/main.js
@@ -0,0 +1,8 @@
+//导入
+const {div,sub} = require("./mathUtil2.js");  //这里是使用了commonjs的导入方式，浏览器无法直接解析，因此需要先通过webpack打包。
+div(10,5);
+
+import {add,mul} from "./mathUtil.js"
+add(10,20)
+mul(10,20)
+
---
+++ Day04/课堂代码/LearnVuejs04/01-webpack的使用/03-webpackd的loader/src/js/mathUtil2.js
@@ -0,0 +1,11 @@
+function div(num1, num2){
+  console.log(num1 / num2);
+}
+function sub(num1, num2){
+  console.log(num1 - num2);
+}
+
+module.exports = {
+  div,
+  sub
+}
